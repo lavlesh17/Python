15fe626878b01e011bd6c35f09a7898be956353e
lavlesh17 <lavleshkumar88@gmail.com>
2026-03-31 12:06:30 +0530

    Question-1

---
+++ Practice/Question-1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first Number"))
+b=int(input("Enter second Number"))
+c=input("Enter the operator")
+if c=="+":
+    print(a+b)
+if c=="-":
+    print(a-b)
+
+if c==r"/":
+    print(a/b)
